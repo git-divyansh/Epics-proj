85a5b500a52d2ab6d29f734be9f5cdaef34df71e
= <divyanshdh@gmail.com>
2023-11-28 12:15:51 +0530

    Update commit

--- dr.medicine/flapi/main.py
+++ dr.medicine/flapi/main.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 
 from flask_cors import CORS
